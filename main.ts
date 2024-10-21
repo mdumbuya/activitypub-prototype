1fd93d9f353850597590103c1c0408e6369d0765
Moses Dumbuya <moses2004112@hotmail.com>
2024-10-21 05:47:40 +0200

    Initial Commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import "$std/dotenv/load.ts";
+
+import { ServerContext } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
+import { behindProxy } from "@hongminhee/x-forwarded-fetch";
+
+const ctx = await ServerContext.fromManifest(manifest, {
+  ...config,
+  dev: false,
+});
+const handler = behindProxy(ctx.handler());
+
+Deno.serve({ handler, ...config.server });
---
+++ routes/_middleware.ts
@@ -0,0 +1,6 @@
+import { Handler } from "$fresh/server.ts";
+import federation from "../federation/mod.ts";
+import { integrateHandler } from "@fedify/fedify/x/fresh";
+
+// This is the entry point to the Fedify middleware from the Fresh framework:
+export const handler: Handler = integrateHandler(federation, () => undefined);
